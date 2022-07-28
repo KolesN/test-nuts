f4ba5e2003ed4beea3ca6c915af3fabf8c65f76f
root <root@DESKTOP-03C6QQU.localdomain>
2022-07-28 18:55:05 +0300

    added part of a script for button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 const newsSlider = document.querySelector('.news-wrapper')
+const news = document.querySelectorAll('.news-content')
+const nextButton = document.querySelector('.next')
+const prevButton = document.querySelector('.prev')
+let currentSlide = 0
+const maxWidth = newsSlider.scrollWidth - newsSlider.clientWidth
+const maxCounter = news.length
 
-console.log('Hi')
+const makeActive = (content, current) => {
+  content.forEach(item => item.classList.remove('active'))
+  content[current].classList.add('active')
+}
+
+nextButton.addEventListener('click', () => {
+  currentSlide = currentSlide === maxCounter - 1 ? 0 : currentSlide + 1
+  newsSlider.scrollLeft =
+    newsSlider.scrollLeft === maxWidth
+      ? newsSlider.scrollLeft - maxWidth
+      : newsSlider.scrollLeft + 250
+  makeActive(news, currentSlide)
+  console.log(newsSlider.scrollLeft)
+})
+
+makeActive(news, currentSlide)
+console.log(maxWidth)
